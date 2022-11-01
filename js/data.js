1c15c7e5f922c9989c333cddc04f99ff66ffa55d
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-01 00:01:41 +0000

    Reset the image preview src attribute. Resest the form inputs.

--- js/data.js
+++ js/data.js
@@ -6,18 +6,3 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-
-var $submitForm = document.querySelector('.entry-form');
-$submitForm.addEventListener('submit', submitEvent);
-
-function submitEvent(event) {
-  var formObject = {};
-
-  event.preventDefault();
-  formObject.title = $submitForm.elements.title.value;
-  formObject.photoUrl = $submitForm.elements['photo-url'].value;
-  formObject.notes = $submitForm.elements.notes.value;
-  formObject.nextEntryId = data.nextEntryId++;
-  data.entries.push(formObject);
-  $submitForm.reset();
-}
--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ function photoUrlInput(event) {
 }
 
 $imageInput.addEventListener('input', photoUrlInput);
+
+var $submitForm = document.querySelector('.entry-form');
+$submitForm.addEventListener('submit', submitEvent);
+
+function submitEvent(event) {
+  var formObject = {};
+
+  event.preventDefault();
+  formObject.title = $submitForm.elements.title.value;
+  formObject.photoUrl = $submitForm.elements['photo-url'].value;
+  formObject.notes = $submitForm.elements.notes.value;
+  formObject.EntryId = data.nextEntryId++;
+  data.entries.push(formObject);
+  $imgView.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $submitForm.reset();
+}
