2c95b41e144adc64b15cd02d58581183ccbffea1
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-01 17:58:35 +0000

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -21,3 +21,57 @@ function submitEvent(event) {
 
 $imageInput.addEventListener('input', photoUrlInput);
 $submitForm.addEventListener('submit', submitEvent);
+
+// DOM tree for list item
+
+function renderEntry(entry) {
+  var entryLi = document.createElement('li');
+
+  var rowDiv = document.createElement('div');
+  rowDiv.setAttribute('class', 'row');
+  entryLi.appendChild(rowDiv);
+
+  var columnImgDiv = document.createElement('div');
+  columnImgDiv.setAttribute('class', 'column-half');
+  rowDiv.appendChild(columnImgDiv);
+
+  var newImg = document.createElement('img');
+  newImg.setAttribute('src', entry.photoUrl);
+  columnImgDiv.appendChild(newImg);
+
+  var columnTextDiv = document.createElement('div');
+  columnTextDiv.setAttribute('class', 'column-half');
+  rowDiv.appendChild(columnTextDiv);
+
+  var newH2 = document.createElement('h2');
+  newH2.textContent = entry.title;
+  columnTextDiv.appendChild(newH2);
+
+  var newP = document.createElement('p');
+  newP.textContent = entry.notes;
+  columnTextDiv.appendChild(newP);
+
+  return entryLi;
+}
+
+var $ulParent = document.querySelector('.entry');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var entry = renderEntry(data.entries[i]);
+  $ulParent.appendChild(entry);
+}
+
+/*
+<li>
+  <div class="row">
+    <div class="column-half">
+      <img src="https://daily.jstor.org/wp-content/uploads/2019/10/ada_lovelace_pioneer_1050x700.jpg">
+    </div>
+    <div class="column-half">
+      <h2>Ada Lovelace</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolores nisi nobis vitae at eos, reprehenderit quod neque libero. Autem distinctio nulla facere culpa omnis est esse quod, voluptate ab?</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est rem vitae facilis quasi voluptas exercitationem quisquam esse beatae, laborum eos incidunt quia veniam, magnam, repellat nam corrupti labore suscipit. Ab!</p>
+    </div>
+  </div>
+</li>
+*/
